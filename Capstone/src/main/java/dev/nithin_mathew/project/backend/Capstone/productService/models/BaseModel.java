f0cb47def323b751d918f7b6c46ce82f8a237605
NithinMathew519 <nithinmathew519@gmail.com>
2025-04-23 14:25:28 -0500

    adding UUID for id  at BaseModel

--- Capstone/src/main/java/dev/nithin_mathew/project/backend/Capstone/productService/models/BaseModel.java
+++ Capstone/src/main/java/dev/nithin_mathew/project/backend/Capstone/productService/models/BaseModel.java
@@ -1,14 +1,19 @@
 package dev.nithin_mathew.project.backend.Capstone.productService.models;
 
-import jakarta.persistence.Id;
-import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.UuidGenerator;
+
+import java.util.UUID;
 
 @Getter
 @Setter
 @MappedSuperclass
 public class BaseModel {
     @Id
-    private Long id;
+    @GeneratedValue
+    @UuidGenerator
+    @Column(name = "id", columnDefinition = "binary(16)", nullable = false, updatable = false)
+    private UUID id;
 }
