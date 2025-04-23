0af8870ee9c99b4269efb35154b3bdb70f1ca743
NithinMathew519 <nithinmathew519@gmail.com>
2025-04-22 19:28:44 -0500

    DB connectivity

--- Capstone/src/main/java/dev/nithin_mathew/project/backend/Capstone/productService/models/BaseModel.java
+++ Capstone/src/main/java/dev/nithin_mathew/project/backend/Capstone/productService/models/BaseModel.java
@@ -1,10 +1,14 @@
 package dev.nithin_mathew.project.backend.Capstone.productService.models;
 
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@MappedSuperclass
 public class BaseModel {
+    @Id
     private Long id;
 }
